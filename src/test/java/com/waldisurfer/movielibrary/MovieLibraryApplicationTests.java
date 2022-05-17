c5bc0fbeea7e4c39154d8c455e1c8cec10e71a3d
Waldi <superkurcze@gmail.com>
2022-05-17 21:35:36 +0200

    Initial commit

---
+++ src/test/java/com/waldisurfer/movielibrary/MovieLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldisurfer.movielibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
